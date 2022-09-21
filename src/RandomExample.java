4a9e7fb603d218ec6ad72292651e87ffb69380c0
Larry Clement <lclement@calbaptist.edu>
2022-09-21 15:54:39 -0700

    After adding comment to RandomeExample.java

--- src/RandomExample.java
+++ src/RandomExample.java
@@ -4,6 +4,7 @@ public class RandomExample {
     public static void main(String[] args) {
         Random rand = new Random();
 
+        // since these all use the same Random object (rand) they will all produce the exact same "random" number
         getRandom(rand);
         getRandom(rand);
         getRandom(rand);
